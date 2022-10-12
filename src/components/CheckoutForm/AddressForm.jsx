395658a7b66615f235e23082191fb638e8824c36
bohdanhulchuknl <bohdanhulchuknl@gmail.com>
2022-10-12 12:44:42 +0300

    2:02:50

--- src/components/CheckoutForm/AddressForm.jsx
+++ src/components/CheckoutForm/AddressForm.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState} from "react";
 
 import {
   Typography,
@@ -13,6 +13,8 @@ import { useForm, FormProvider } from "react-hook-form";
 import FormInput from "./FormInput";
 
 const AddressForm = () => {
+  const [shippingCountries, setShippingCountries] = useState([])
+  const [shippingCountry, setShippingCountry] = useState([])
   const methods = useForm();
   return (
     <>
@@ -23,6 +25,35 @@ const AddressForm = () => {
         <form onSubmit={() => console.log("submit")}>
           <Grid container spacing={3}>
             <FormInput required={true} name="firstName" label="First name" />
+            <FormInput required={true} name="lastName" label="Last name" />
+            <FormInput required={true} name="address1" label="Address" />
+            <FormInput required={true} name="email" label="Email" />
+            <FormInput required={true} name="city" label="City" />
+            <FormInput required={true} name="zip" label="ZIP / Postal code" />
+            <Grid item xs={12} sm={6}>
+              <InputLabel>Shipping Country</InputLabel>
+              <Select value={} fullWidth onChange={}>
+                <MenuItem key={} value={}>
+                  Select me
+                </MenuItem>
+              </Select>
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <InputLabel>Shipping Subdivision</InputLabel>
+              <Select value={} fullWidth onChange={}>
+                <MenuItem key={} value={}>
+                  Select me
+                </MenuItem>
+              </Select>
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <InputLabel>Shipping Options</InputLabel>
+              <Select value={} fullWidth onChange={}>
+                <MenuItem key={} value={}>
+                  Select me
+                </MenuItem>
+              </Select>
+            </Grid>
           </Grid>
         </form>
       </FormProvider>
--- src/components/CheckoutForm/FormInput.jsx
+++ src/components/CheckoutForm/FormInput.jsx
@@ -5,16 +5,15 @@ import { useFormContext, Controller } from "react-hook-form";
 
 const FormInput = ({ name, label, required }) => {
   const { control } = useFormContext();
-
+  console.log(name, label, required);
   return (
     <Grid item xs={12} sm={6}>
       <Controller
-        as={TextField}
         control={control}
-        fullWidth
         name={name}
-        label={label}
-        required={required}
+        render={({ field }) => (
+          <TextField fullWidth label={label} required={required} />
+        )}
       />
     </Grid>
   );
